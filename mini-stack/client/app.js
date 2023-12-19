d29139489b89ef03f29bb520bff2385618c3e126
Myles Billignton <mylesbillington@gmail.com>
2023-12-19 11:15:56 +0000

    git init

---
+++ mini-stack/client/app.js
@@ -0,0 +1,14 @@
+// call my api
+async function getMessage(){
+    // call our API with fetch
+    const response = await fetch('http://localhost:8080/message');
+
+    // turn the response into json
+    const json = await response.json();
+
+    // put my message on the page
+    const p = document.getElementById('message');
+    p.textContent = json.message;
+}
+
+getMessage();
